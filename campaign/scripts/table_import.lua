4dacf60f189e70af774e28f5bc0e28c15905dffd
MikeW <uce_mike@yahoo.com>
2018-11-26 22:10:11 -0600

    Added support for "XX-XX String of Text" and just "String of Text" and it will use XX-XX or XX for the dice roll
    Added support for "XX-XX String of Text" and just "String of Text" and it will use XX-XX or XX for the dice roll

--- campaign/scripts/table_import.lua
+++ campaign/scripts/table_import.lua
@@ -28,26 +28,51 @@ function importTextAsTable()
     local sText = tableimporttext.getValue() or "";
 --Debug.console("table_import.lua","importTextAsTable","sText",sText);    
     if (sText ~= "") then
-        local aTableText = {};
-        for sLine in string.gmatch(sText, '([^\r\n]+)') do
-            table.insert(aTableText, sLine);
-        end
-        local nodeTable = createBlankTable();
-        DB.setValue(nodeTable,"name","string",aTableText[1]);
-        local nNewRow = 0;
-        if (nodeTable) then
-            local nodeTableRows = nodeTable.createChild("tablerows");
-            for _,sTableLines in ipairs(aTableText) do
-                nNewRow = nNewRow + 1;
-                if (nNewRow ~= 1) then -- use line 1 as title/name of table
-                    local nodeRow = nodeTableRows.createChild();
-                    DB.setValue(nodeRow, "fromrange", "number", nNewRow-1);
-                    DB.setValue(nodeRow, "torange", "number", nNewRow-1);
-                    local nodeResults = nodeRow.createChild("results");
-                    local nodeResult = nodeResults.createChild();
-                    DB.setValue(nodeResult, "result", "string", sTableLines);
-                end
+      local aTableText = {};
+      for sLine in string.gmatch(sText, '([^\r\n]+)') do
+        table.insert(aTableText, sLine);
+      end
+      local nodeTable = createBlankTable();
+      DB.setValue(nodeTable,"name","string",aTableText[1]);
+      local nNewRow = 0;
+      if (nodeTable) then
+        local nodeTableRows = nodeTable.createChild("tablerows");
+        for _,sTableLines in ipairs(aTableText) do
+          local nStart, nLast = 0;
+Debug.console("table_import.lua","importTextAsTable","sTableLines",sTableLines);          
+          local sStart, sLast, sText = sTableLines:match("^(%d+)-(%d+) (.*)"); -- grab 1-10 numbers
+Debug.console("table_import.lua","importTextAsTable","sStart",sStart);          
+Debug.console("table_import.lua","importTextAsTable","sLast",sLast);
+Debug.console("table_import.lua","importTextAsTable","sText",sText);
+          if (sStart == nil or sLast == nil) then -- check for just a single starting number then
+            sStart, sText = sTableLines:match("^(%d+) (.*)");
+          end
+          if (sStart ~= nil) then -- we got at least 1 number
+            if sLast == nil then  -- just a single number
+              sLast = sStart;
             end
+            nStart = tonumber(sStart) or 1;
+            nLast = tonumber(sLast) or 1;
+            nNewRow = nNewRow + 1;
+          else
+Debug.console("table_import.lua","importTextAsTable","NO # MATCH---sTableLines--->",sTableLines);
+            sText = sTableLines;
+            nNewRow = nNewRow + 1;
+            nStart = nNewRow-1;
+            nLast = nNewRow-1;
+          end
+Debug.console("table_import.lua","importTextAsTable","nStart",nStart);          
+Debug.console("table_import.lua","importTextAsTable","nLast",nLast);
+Debug.console("table_import.lua","importTextAsTable","nNewRow",nNewRow);
+          if (nNewRow ~= 1) then -- use line 1 as title/name of table
+            local nodeRow = nodeTableRows.createChild();
+            DB.setValue(nodeRow, "fromrange", "number", nStart);
+            DB.setValue(nodeRow, "torange", "number", nLast);
+            local nodeResults = nodeRow.createChild("results");
+            local nodeResult = nodeResults.createChild();
+            DB.setValue(nodeResult, "result", "string", sText);
+          end
         end
+      end
     end
 end
