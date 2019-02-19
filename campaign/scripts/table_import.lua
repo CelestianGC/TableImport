96f91f7f8fbd189516e6e4edc378932aa217088a
MikeW <uce_mike@yahoo.com>
2019-02-18 21:55:08 -0600

    Split on %s%s+ and add multiple cols for each

--- campaign/scripts/table_import.lua
+++ campaign/scripts/table_import.lua
@@ -30,10 +30,13 @@ function importTextAsTable()
     if (sText ~= "") then
       local aTableText = {};
       for sLine in string.gmatch(sText, '([^\r\n]+)') do
+        sLine = StringManager.trim(sLine);  -- remove spaces in front and on end.
         table.insert(aTableText, sLine);
       end
       local nodeTable = createBlankTable();
       DB.setValue(nodeTable,"name","string",aTableText[1]);
+      DB.setValue(nodeTable,"resultscols","number",1);
+      local nResultsCols = 1;
       local nNewRow = 0;
       if (nodeTable) then
         local nodeTableRows = nodeTable.createChild("tablerows");
@@ -57,14 +60,45 @@ function importTextAsTable()
             nLast = nNewRow-1;
           end
           if (nNewRow ~= 1) then -- use line 1 as title/name of table
+            --local aText = StringManager.split(sText," %s+",true);
+            --local aText = string.gmatch(sText," %s+");
+            local aText = split(sText,"%s%s+");
+--Debug.console("table_import.lua","importTextAsTable","aText",aText);
+--Debug.console("table_import.lua","importTextAsTable","#aText",#aText);
+            if #aText > nResultsCols then
+              DB.setValue(nodeTable,"resultscols","number",#aText);
+              nResultsCols = #aText;
+            end
             local nodeRow = nodeTableRows.createChild();
             DB.setValue(nodeRow, "fromrange", "number", nStart);
             DB.setValue(nodeRow, "torange", "number", nLast);
             local nodeResults = nodeRow.createChild("results");
-            local nodeResult = nodeResults.createChild();
-            DB.setValue(nodeResult, "result", "string", sText);
+            for i=1, #aText do
+              local nodeResult = nodeResults.createChild();
+              DB.setValue(nodeResult, "result", "string", StringManager.trim(aText[i]));
+            end
           end
         end
       end
     end
+end
+
+-- this allows me to split on 2+spaces
+function split(str, pat)
+  local t = {};
+  local fpat = "(.-)" .. pat;
+  local last_end = 1;
+  local s, e, cap = str:find(fpat, 1);
+  while s do
+  if s ~= 1 or cap ~= "" then
+   table.insert(t,cap);
+  end
+    last_end = e+1;
+    s, e, cap = str:find(fpat, last_end);
+  end
+  if last_end <= #str then
+    cap = str:sub(last_end);
+    table.insert(t, cap);
+  end
+  return t
 end
